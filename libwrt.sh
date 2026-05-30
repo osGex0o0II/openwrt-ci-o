a06ba08df19c707451b08d338a2161cde79b0ae9
#eaB@Il <108638675+osGex0o0II@users.noreply.github.com>
2026-05-31 04:44:11 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -4,15 +4,7 @@
 # =======================================================
 
 echo "========== 注入第三方 Aurora 主题 =========="
+# 将 Aurora 主题源码克隆到 package 目录下，让 OpenWrt 编译系统能够识别并编译它
 git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-
-echo "========== 彻底剔除默认 Bootstrap 主题 =========="
-# 1. 删除已安装到 package 目录的 bootstrap
-rm -rf package/feeds/luci/luci-theme-bootstrap
-# 2. 删除 feeds 源码目录的 bootstrap
-rm -rf feeds/luci/themes/luci-theme-bootstrap
-# 3. 斩断 luci 核心包对 bootstrap 的强制依赖 (防止 make defconfig 强行拉回)
-sed -i 's/+luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
-sed -i 's/+luci-theme-bootstrap//g' feeds/luci/modules/luci-base/Makefile
 
 echo "========== DIY 脚本执行完毕 =========="
