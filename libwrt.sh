df180239ff9ae9f6afc1b461e4a3443b9a072f33
#eaB@Il <108638675+osGex0o0II@users.noreply.github.com>
2026-05-31 01:33:30 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,10 @@
-rm -rf package/emortal/luci-app-athena-led
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+#!/bin/bash
+# =======================================================
+# 自定义 DIY 脚本 (针对 兆能 M2 纯有线主路由)
+# =======================================================
+
+echo "========== 开始注入第三方 Aurora 主题 =========="
+# 将 Aurora 主题源码克隆到 package 目录下，让 OpenWrt 编译系统能够识别并编译它
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+
+echo "========== DIY 脚本执行完毕 =========="
