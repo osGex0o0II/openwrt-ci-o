36d95d12c926c0d3b9e5e22e03a659efb2a81c88
osGex0o0II <osGex0o0II@users.noreply.github.com>
2026-06-02 04:36:42 +0800

    chore: add .gitignore, remove unused images, sync script comment

--- files-256m/etc/uci-defaults/98-network-performance.sh
+++ files-256m/etc/uci-defaults/98-network-performance.sh
@@ -6,6 +6,8 @@ uci -q set dhcp.@dnsmasq[0].min_cache_ttl='1800'
 uci -q set dhcp.@dnsmasq[0].allservers='1'
 uci -q commit dhcp
 
+# File descriptor limits for proxy workloads. Keep this idempotent because
+# uci-defaults scripts can be retried if any earlier command fails.
 mkdir -p /etc/security
 touch /etc/security/limits.conf
 grep -qF '* soft nofile 32768' /etc/security/limits.conf || echo '* soft nofile 32768' >> /etc/security/limits.conf
